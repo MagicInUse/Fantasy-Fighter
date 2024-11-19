3510fbe9baff0347ef6d6295e11f9329e24d89cf
Cinnlight <cinnlight@gmail.com>
2024-11-19 16:29:53 -0700

    Comments out the contents of this index.ts - to be replaced by index.ts within /src/routes/

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
-import express from 'express';
+// import express from 'express';
 
-const app = express();
-const port = process.env.PORT || 3000;
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get('/', async (req, res) => {
-  // TODO: add sequelize
-});
+// app.get('/', async (req, res) => {
+//   // TODO: add sequelize
+// });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
