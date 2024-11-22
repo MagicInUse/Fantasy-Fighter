045d693861150773c84930675c98d2ef8da34402
Kristen Shields <shieldskristen0@gmail.com>
2024-11-21 20:41:29 -0700

    updated enemy.ts code to link with level.ts

--- server/src/models/enemy.ts
+++ server/src/models/enemy.ts
@@ -1,15 +1,18 @@
-import { Model, DataTypes } from "sequelize";
+import { Model, DataTypes, Sequelize } from "sequelize";
+import { Level } from "./level";
 
 interface EnemyAttributes {
     health: number;
     damage: number;
+    levelId?: number;
 }
 
 export class Enemy extends Model<EnemyAttributes> implements EnemyAttributes {
     public health!: number;
     public damage!: number;
+    public levelId!: number;
 }
-export function EnemyFactory(sequelize: typeof Sequelize): typeof Enemy {
+export function EnemyFactory(sequelize: Sequelize): typeof Enemy {
 
     Enemy.init(
         {
@@ -20,6 +23,14 @@ export function EnemyFactory(sequelize: typeof Sequelize): typeof Enemy {
             damage: {
                 type: DataTypes.INTEGER,
                 allowNull: false,
+            },
+            levelId: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: 'level',
+                    key: 'level_id',
+
+                },
             },
         },
         {
