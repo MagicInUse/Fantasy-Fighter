467d2ad3657e4268f800d2e2c1d392c705b580a3
MagicInUse <jacob.norr@live.com>
2024-11-26 23:41:27 -0700

    sequelize Render deployment tweaks

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -45,6 +45,7 @@ export function UserFactory(sequelize: Sequelize): typeof User {
         {
             modelName: "User",
             tableName: "users",
+            timestamps: false,
             sequelize,
         }
     );
