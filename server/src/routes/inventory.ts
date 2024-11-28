72fe36cdf603e315f4a22c35afc7eb99b25332ce
MagicInUse <jacob.norr@live.com>
2024-11-27 21:21:28 -0700

    worked on some combat api. got sidetracked off to inventory land finally landing back on levels somehow. worked out server inventory seeding and uncommented the get request for itemData. either way, Inventory is using api logic to pull the items from the server, not using a local variable anymore.

--- server/src/routes/inventory.ts
+++ server/src/routes/inventory.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from 'express';
 import { Item } from '../models/index';
+
 const router = express.Router();
+
+const itemData = [
+    { id: 1, itemName: 'Bow', description: 'A ranged weapon that fires iron-tipped arrows', type: 1, quantity: 1, damage: Math.floor(Math.random() * 10) + 1 },
+    { id: 2, itemName: 'Sword', description: 'A sharp blade used for combat.', type: 1, quantity: 1, damage: Math.floor(Math.random() * 6) + 3 },
+    { id: 3, itemName: 'Health Potion', description: 'A swirling green liquid.', type: 2, quantity: 3, effect: 'Heals 10 health' },
+    { id: 4, itemName: 'Mana Potion', description: 'A thick blue substance.', type: 3, quantity: 1, effect: 'Restores 10 mana' },
+];
 
 // Item seed data
 export const createItems = async (req: Request, res: Response): Promise<void> => {
-    const itemData = [
-        { id: 1, itemName: 'Bow', description: 'A ranged weapon that fires iron-tipped arrows', type: 1, quantity: 1, damage: Math.floor(Math.random() * 10) + 1 },
-        { id: 2, itemName: 'Sword', description: 'A sharp blade used for combat.', type: 2, quantity: 1, damage: Math.floor(Math.random() * 6) + 3 }
-    ];
 
     try {
         const existingItemsCount = await Item.count();
@@ -26,9 +30,9 @@ export const createItems = async (req: Request, res: Response): Promise<void> =>
 
 // GET /api/inventory 
 // Gets all inventory items
-// router.get('/', (req, res) => {
-//     res.json(itemData);
-// });
+router.get('/', (req, res) => {
+    res.json(itemData);
+});
 
 // POST /api/inventory 
 // Add item to inventory
