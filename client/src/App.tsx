686e598145aea6b655fb408438452c82fccd96c0
MagicInUse <jacob.norr@live.com>
2024-11-30 04:57:26 -0700

    test fail, removing tainted code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
-import auth from './utils/auth';
 
 function App() {
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    const checkLogin = () => {
-      if (!auth.loggedIn()) {
-        auth.logout();
-        navigate('/');
-      }
-    };
-
-    checkLogin();
-  }, [navigate]);
-  
   return (
     <div>
       <Navbar />
