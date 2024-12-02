0cfe7ac12fa3857473dbba62ca4886a35f8039e3
CinnlShiro <cinnlight@gmail.com>
2024-12-01 21:54:53 -0700

    Update getItem api call to use an updated route

--- client/src/api/itemAPI.tsx
+++ client/src/api/itemAPI.tsx
@@ -3,12 +3,13 @@ import type { ItemData } from '../interfaces/ItemData';
 import AuthService from '../utils/auth';
 
 // GET /api/inventory endpoint to get all items
-const getItems = async (): Promise<ItemData[]> => {
+// Function to fetch items from the server
+const getItems = async (): Promise<ItemData> => {
   if (!AuthService.loggedIn()) {
     return Promise.reject('User is not authenticated');
   }
 
-  // Fetch all items with user authentication
+  // Fetch player data from the server with user authentication
   try {
     const response = await fetch('/api/inventory', {
       headers: {
@@ -17,15 +18,15 @@ const getItems = async (): Promise<ItemData[]> => {
     });
 
     if (!response.ok) {
-      throw new Error('Could not retrieve items');
+      throw new Error('Could not retrieve player data');
     }
 
     const data = await response.json();
     return data;
 
   } catch (err) {
-    console.log('Error from item API: ', err);
-    return Promise.reject('Could not fetch items');
+    console.log('Error from combat API: ', err);
+    return Promise.reject('Could not fetch player data');
   }
 };
 
